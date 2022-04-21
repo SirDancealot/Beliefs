25016422b8be20849b5ce616569dbb4f95c67e3b
Eraisuithon <mj@lpj-software.dk>
2022-04-21 16:53:55 +0200

    Started implementing classes for each operation

---
+++ beliefs/src/main/java/ai/gr64/belief/operations/ElementsInSet.java
@@ -0,0 +1,14 @@
+package ai.gr64.belief.operations;
+
+import ai.gr64.belief.Possibilities;
+import ai.gr64.belief.interfaces.IOpp;
+
+public class ElementsInSet implements IOpp {
+
+    @Override
+    public Possibilities evaluate() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
