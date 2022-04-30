01e6a21259fb17dbcc87c49e3b56b9a610596473
Eraisuithon <mj@lpj-software.dk>
2022-04-30 21:08:55 +0200

    Made text update when pressing a button

--- beliefs/src/main/java/ai/gr64/MainMenuController.java
+++ beliefs/src/main/java/ai/gr64/MainMenuController.java
@@ -1,6 +1,8 @@
 package ai.gr64;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import ai.gr64.belief.interfaces.IOpp;
 import ai.gr64.belief.operations.And;
@@ -11,13 +13,15 @@ import ai.gr64.belief.operations.Unit;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.IndexRange;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class MainMenuController {
+public class MainMenuController implements Initializable {
 
     @FXML
     private Button notButton;
@@ -41,28 +45,65 @@ public class MainMenuController {
     private static IOpp k = new Parenthesis();
     private static IOpp i, p, q;
 
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        baseTextField.setText("⬚");
+    }
+
     @FXML
     public void handleNot(ActionEvent event) {
+        if (checkFull())
+            return;
         k.addOpp(new Not());
+        updateBaseText("¬⬚");
     }
 
     @FXML
     public void handleAnd(ActionEvent event) {
+        if (checkFull())
+            return;
         k.addOpp(new And());
+        updateBaseText("⬚∧☐");
     }
 
     @FXML
     public void handleOr(ActionEvent event) {
+        if (checkFull())
+            return;
         k.addOpp(new Or());
+        updateBaseText("⬚∨☐");
     }
 
     @FXML
     public void handleParenthesis(ActionEvent event) {
+        if (checkFull())
+            return;
         k.addOpp(new Parenthesis());
+        updateBaseText("(⬚)");
     }
 
     public void unitAdded(Unit unit) {
+        if (checkFull())
+            return;
         k.addOpp(unit);
+        updateBaseText("U");
+        String text = baseTextField.getText();
+        baseTextField.setText(text.replaceFirst("☐", "⬚"));
+    }
+
+    private boolean checkFull() {
+        if (k.isFull()) {
+
+            return true;
+        }
+        return false;
+    }
+
+    private void updateBaseText(String replacement) {
+        String org = baseTextField.getText();
+
+        baseTextField.setText(org.replace("⬚", replacement));
+
     }
 
     @FXML
@@ -92,7 +133,7 @@ public class MainMenuController {
             newWindow.setY(stage.getY() + 100);
 
             newWindow.show();
-            
+
         } catch (IOException e) {
             e.printStackTrace();
         }
