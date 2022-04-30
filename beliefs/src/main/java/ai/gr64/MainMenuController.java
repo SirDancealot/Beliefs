65c827c67888f1fe6464d255e5e443970dba87a4
Eraisuithon <mj@lpj-software.dk>
2022-04-30 14:42:33 +0200

    Fixed new window from handleUnit

--- beliefs/src/main/java/ai/gr64/MainMenuController.java
+++ beliefs/src/main/java/ai/gr64/MainMenuController.java
@@ -36,11 +36,9 @@ public class MainMenuController {
     private TextField qubeTextField;
     @FXML
     private TextField iTextField;
-    
 
+    private static IOpp i, k, p, q;
 
-    private static IOpp i ,k, p, q;
-    
     @FXML
     public void handleNot(ActionEvent event) {
         i.addOpp(new Not());
@@ -60,32 +58,32 @@ public class MainMenuController {
     public void handleParenthesis(ActionEvent event) {
         i.addOpp(new Parenthesis());
     }
-    
+
     @FXML
     public void handleUnit(ActionEvent event) {
         Stage stage = (Stage) (((Button) event.getSource()).getScene().getWindow());
-        Scene secondScene = new Scene(loadFXML("inputPopUp"), 600, 400);
+        try {
+            Scene secondScene = new Scene(App.loadFXML("inputPopUp"), 600, 400);
+            // New window (Stage)
+            Stage newWindow = new Stage();
+            newWindow.setTitle("Second Stage");
+            newWindow.setScene(secondScene);
 
-        // New window (Stage)
-        Stage newWindow = new Stage();
-        newWindow.setTitle("Second Stage");
-        newWindow.setScene(secondScene);
+            // Specifies the modality for new window.
+            newWindow.initModality(Modality.WINDOW_MODAL);
 
-        // Specifies the modality for new window.
-        newWindow.initModality(Modality.WINDOW_MODAL);
+            // Specifies the owner Window (parent) for new window
+            newWindow.initOwner(stage);
 
-        // Specifies the owner Window (parent) for new window
-        newWindow.initOwner(stage);
+            // Set position of second window, related to primary window.
+            newWindow.setX(stage.getX() + 200);
+            newWindow.setY(stage.getY() + 100);
 
-        // Set position of second window, related to primary window.
-        newWindow.setX(stage.getX() + 200);
-        newWindow.setY(stage.getY() + 100);
+            newWindow.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        newWindow.show();
-    }
-
-    private Parent loadFXML(String string) {
-        return null;
     }
 
     @FXML
@@ -95,18 +93,12 @@ public class MainMenuController {
 
     @FXML
     public void handleRevision(ActionEvent event) {
-        
+
     }
 
-    
     public static void gui() {
         i = new Parenthesis();
-        
-        
-        
-        
-        
+
     }
 
-        
 }
